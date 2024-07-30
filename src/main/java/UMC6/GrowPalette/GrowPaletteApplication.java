9c6057306011136e9187a2301dfc4a93958c046e
joon <yijiyoon06@gmail.com>
2024-07-30 21:00:04 +0900

    Initial commit with Spring project

---
+++ src/main/java/UMC6/GrowPalette/GrowPaletteApplication.java
@@ -0,0 +1,13 @@
+package UMC6.GrowPalette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowPaletteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowPaletteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/UMC6/GrowPalette/GrowPaletteApplicationTests.java
@@ -0,0 +1,13 @@
+package UMC6.GrowPalette;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowPaletteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
