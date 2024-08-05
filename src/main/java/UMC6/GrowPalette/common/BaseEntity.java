e3fe565fb3e6e3f07dd305ce6ba939121ee34811
joon <yijiyoon06@gmail.com>
2024-08-05 11:53:59 +0900

    .gitignore, build.gradle, apipayload 파일 수정

--- src/main/java/UMC6/GrowPalette/common/BaseEntity.java
+++ src/main/java/UMC6/GrowPalette/common/BaseEntity.java
@@ -1,8 +1,20 @@
 package UMC6.GrowPalette.common;
 
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import java.time.LocalDateTime;
 
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
+    @CreatedDate
     private LocalDateTime createdAt;
+    @LastModifiedDate
     private LocalDateTime updatedAt;
 }
